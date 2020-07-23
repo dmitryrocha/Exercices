f10f14d9d9deb291b994aa4c8481562524754af2
Dmitry Rocha <dmitry.silva@ucsal.edu.br>
2020-07-23 15:41:13 -0300

    Objective In this challenge, we're going to use loops to help us do some simple math. Check out the Tutorial tab to learn more.
    Task
    Given an integer, , print its first  multiples. Each multiple  (where ) should be printed on a new line in the form: n x i = result.
    
    Input Format
    
    A single integer, .
    
    Constraints
    
    Output Format
    
    Print  lines of output; each line  (where ) contains the  of  in the form:
    n x i = result.

--- src/hackerRank30Days/Dia05.java
+++ src/hackerRank30Days/Dia05.java
@@ -6,11 +6,14 @@ public class Dia05 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Qual o número?");
-        int n = sc.nextInt();
 
-        for(int i = 1; i <= 10; i++) {
-            int resultado = n*i;
-            System.out.println(n+" x "+i+" = "+resultado);
+        int n = sc.nextInt();
+        if(n<=2 && n >=20) {
+            for (int i = 1; i <= 10; i++) {
+                int resultado = n * i;
+                System.out.println(n + " x " + i + " = " + resultado);
+
+            }
         }
     }
 }
