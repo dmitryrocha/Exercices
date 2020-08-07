ffd3809c861acafa9dcb06bf7a695719b38d7660
Dmitry Rocha <dmitry.silva@ucsal.edu.br>
2020-08-07 13:38:21 -0300

    Task Given an array, , of size  distinct elements, sort the array in ascending order using the Bubble Sort algorithm above. Once sorted, print the following  lines:
    Array is sorted in numSwaps swaps.
    where  is the number of swaps that took place.
    First Element: firstElement
    where  is the first element in the sorted array.
    Last Element: lastElement
    where  is the last element in the sorted array.
    Hint: To complete this challenge, you will need to add a variable that keeps a running tally of all swaps that occur during execution.
    
    Input Format
    
    The first line contains an integer, , denoting the number of elements in array .
    The second line contains  space-separated integers describing the respective values of .
    
    Constraints
    
    , where .
    Output Format
    
    Print the following three lines of output:
    
    Array is sorted in numSwaps swaps.
    where  is the number of swaps that took place.
    First Element: firstElement
    where  is the first element in the sorted array.
    Last Element: lastElement
    where  is the last element in the sorted array.
    Sample Input 0
    
    3
    1 2 3
    Sample Output 0
    
    Array is sorted in 0 swaps.
    First Element: 1
    Last Element: 3
    Explanation 0
    
    The array is already sorted, so  swaps take place and we print the necessary  lines of output shown above.
    
    Sample Input 1
    
    3
    3 2 1
    Sample Output 1
    
    Array is sorted in 3 swaps.
    First Element: 1
    Last Element: 3

---
+++ src/hackerRank30Days/Day20.java
@@ -0,0 +1,36 @@
+package hackerRank30Days;
+
+
+public class Day20 {
+    public static void main(String[] args) {
+        int[] array = {5,4,2,1};
+        int [] array2 = {1,2,3};
+        int[] array3 = {3,2,1};
+
+        System.out.println("Arrumar a primeira:");
+        arrumador(array);
+        System.out.println("Arrumar a segunda:");
+        arrumador(array2);
+        System.out.println("Arrumar a terceira:");
+        arrumador(array3);
+
+    }
+
+    public static void arrumador (int[] array) {
+        int mudadas = 0;
+
+        for(int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length-1; j++) {
+                if(array[j]>array[j+1]){
+                    int temp = array[j+1];
+                    array[j+1] = array[j];
+                    array[j] = temp;
+                    mudadas++;
+                }
+            }
+        }
+        System.out.println("Array is sorted in "+mudadas+ " swaps.");
+        System.out.println("First Element: "+array[0]);
+        System.out.println("Last Element: "+array[array.length-1]);
+    }
+}
