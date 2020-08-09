3379e11df059dbaf3b26ccea783d3174a43be95e
Dmitry Rocha <dmitry.silva@ucsal.edu.br>
2020-08-09 18:23:41 -0300

    Task The height of a binary search tree is the number of edges between the tree's root and its furthest leaf. You are given a pointer, , pointing to the root of a binary search tree. Complete the getHeight function provided in your editor so that it returns the height of the binary search tree.
    Input Format
    
    The locked stub code in your editor reads the following inputs and assembles them into a binary search tree:
    The first line contains an integer, , denoting the number of nodes in the tree.
    Each of the  subsequent lines contains an integer, , denoting the value of an element that must be added to the BST.
    
    Output Format
    
    The locked stub code in your editor will print the integer returned by your getHeight function denoting the height of the BST.
    
    Sample Input
    
    7
    3
    5
    2
    1
    4
    6
    7
    Sample Output
    
    3

---
+++ src/hackerRank30Days/Dia22.java
@@ -0,0 +1,82 @@
+package hackerRank30Days;
+
+import java.util.Scanner;
+
+public class Dia22 {
+    public static int getHeight(NoHR root){
+        NoHR temp = root;
+        int altura = 0;
+        int aux = 0;
+        boolean sair = false;
+        while(!sair) {
+            if(temp.left != null) {
+                temp = temp.left;
+                aux++;
+            } else if (temp.right != null) {
+                temp = temp.right;
+                aux++;
+            } else {
+                sair = true;
+            }
+        }
+        temp = root;
+        altura = aux;
+        aux = 0;
+        sair = false;
+        while(!sair) {
+            if(temp.right != null){
+                temp = temp.right;
+                aux++;
+            } else if (temp.left != null) {
+                temp = temp.left;
+                aux++;
+            } else {
+                sair = true;
+            }
+
+        }
+        if(altura > aux) {
+            return altura;
+        } else {
+            return  aux;
+        }
+    }
+
+    public static NoHR insert(NoHR root, int data){
+        if(root==null){
+            return new NoHR(data);
+        }
+        else{
+            NoHR cur;
+            if(data<=root.data){
+                cur=insert(root.left,data);
+                root.left=cur;
+            }
+            else{
+                cur=insert(root.right,data);
+                root.right=cur;
+            }
+            return root;
+        }
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int T=sc.nextInt();
+        NoHR root=null;
+        while(T-->0){
+            int data=sc.nextInt();
+            root=insert(root,data);
+        }
+        int height=getHeight(root);
+        System.out.println(height);
+    }
+}
+
+class NoHR {
+    NoHR left,right;
+    int data;
+    NoHR(int data){
+        this.data=data;
+        left=right=null;
+    }
+}
